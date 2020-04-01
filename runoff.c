7e8df9df58f14cc02aacfe89b10b2d1d6552ed8c
kennbp31 <kennbp31@users.noreply.github.com>
2020-04-01 23:39:29 +0000

    automated commit by check50 [check50=True]

--- runoff.c
+++ runoff.c
@@ -144,6 +144,7 @@ void tabulate(void)
 {
     for (int i = 0; i < candidate_count; i++)
     {
+        candidates[i].votes = 0;
         if (candidates[i].eliminated == false)
         {
         for (int j = 0; j < voter_count; j++)
