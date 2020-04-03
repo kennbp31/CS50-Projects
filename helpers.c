f1cf258384b11b7e583c10652b3e0ac882f241a4
kennbp31 <kennbp31@users.noreply.github.com>
2020-04-03 03:01:10 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -20,6 +20,46 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0, temp_r, temp_b, temp_g; j < width; j++)
+        {
+            temp_r = round(.393 * (float)image[i][j].rgbtRed + .769 * (float)image[i][j].rgbtGreen + .189 * (float)image[i][j].rgbtBlue);
+            temp_g = round(.349 * (float)image[i][j].rgbtRed + .686 * (float)image[i][j].rgbtGreen + .168 * (float)image[i][j].rgbtBlue);
+            temp_b = round(.272 * (float)image[i][j].rgbtRed + .534 * (float)image[i][j].rgbtGreen + .131 * (float)image[i][j].rgbtBlue);
+
+            if (temp_r > 255)
+            {
+                image[i][j].rgbtRed = 255;
+            }
+
+            else
+            {
+                image[i][j].rgbtRed = temp_r;
+            }
+
+            if (temp_g > 255)
+            {
+                image[i][j].rgbtGreen = 255;
+            }
+
+            else
+            {
+                image[i][j].rgbtGreen = temp_g;
+            }
+
+            if (temp_b > 255)
+            {
+                image[i][j].rgbtBlue = 255;
+            }
+
+            else
+            {
+                image[i][j].rgbtBlue = temp_b;
+            }
+
+        }
+    }
     return;
 }
 
