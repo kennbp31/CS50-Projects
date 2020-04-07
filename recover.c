102539e693f5de90cd01321c780f66f852f8bc03
kennbp31 <kennbp31@users.noreply.github.com>
2020-04-07 13:09:35 +0000

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -27,62 +27,45 @@ int main(int argc, char *argv[])
     fseek(file, 0, SEEK_END);// seek to end of file
     b_count = ftell(file);// get current file pointer
     fseek(file, 0, SEEK_SET);//go back to beginning of file
-    char jpg_name[8] = "";
+    char jpg_name[8] = "000.jpg";
 
-
-    BYTE mem[b_count];//temp location for the file in the stack
-    BYTE mem_2[b_count];
+    BYTE mem[b_count];
+    //mem = (BYTE*) malloc(b_count);//temp location for the file in the stack
     
-    
-    
-    
-    fread(mem, sizeof(BYTE),b_count,file);
-    fseek(file, 0, SEEK_SET);
-    int seek_start = 0;
-    int file_size;
-
-    for (int i = 0; i < b_count; i++)
+    fread(mem, sizeof(BYTE), b_count, file);
+    fclose(file);
+    int j; 
+    for (int i = 0; i < b_count; i = i + 512)
     {
-       mem_2[i] = mem[i]; 
-    }
-    
-    for (int i = 0; i < b_count; i++, seek_start++)
-    {
-        
-        file_size = 0;
         if (mem[i] == 0xff && mem[i + 1] == 0xd8 && mem[i + 2] == 0xff && (mem[i + 3] & 0xf0) == 0xe0)
         {
             if (jpg < 10)
             {
                 sprintf(jpg_name, "00%i.jpg", jpg);
-               // printf("%s\n", jpg_name); 
             }
             
             else if (jpg > 9)
             {
                 sprintf(jpg_name, "0%i.jpg", jpg);
-                //printf("%s\n", jpg_name);
             }
             
             to_write = fopen(jpg_name, "a");//open a new file to write too
-            
-            int j = i;
-            while (!(mem_2[j + 1] == 0xff && mem_2[j + 2] == 0xd8 && mem_2[j + 3] == 0xff && (mem_2[j + 4] & 0xf0) == 0xe0))
-            
+            j = i;
+            while(!(mem[j + 1] == 0xff && mem[j + 2] == 0xd8 && mem[j + 3] == 0xff && (mem[j + 4] & 0xf0) == 0xe0) && j + 4 < b_count)
             {
-                 
-                 fwrite(&mem[j], sizeof(BYTE),1, to_write);
-                 file_size++;
-                 j++;
-            }
+                fwrite(&mem[j], sizeof(BYTE), 1, to_write);
+                j++;
+                // fputc(mem[j],to_write);
+           }
             
+            
+        
             //printf("File Size:%i\n", file_size);
-            fclose(to_write);//CLOSE JPEG
+            //CLOSE JPEG
             jpg++; //advance jpg naming counter
             //printf("%x %x %x %x \n", mem[i], mem[i+1], mem[i+2], mem[i+3]);
         }
     }
-
-    fclose(file);
-    printf("BYTES: %i\njpgs: %i\n", b_count, jpg);
+    //free(mem);
+    fclose(to_write);
 }
